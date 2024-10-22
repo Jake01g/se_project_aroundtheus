88a5ad555867fd3aa2c381e08151486bce05227c
Jake Glans <165740770+Jake01g@users.noreply.github.com>
2024-10-22 15:49:48 -0700

    setInputVal Added

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -16,6 +16,12 @@ export default class PopupWithForm extends Popup {
     return formValues;
   }
 
+  setInputValues(data) {
+    this._inputList.forEach((input) => {
+      input.value = data[input.name];
+    });
+  }
+
   setEventListeners() {
     this._popupForm.addEventListener("submit", (evt) => {
       evt.preventDefault();
