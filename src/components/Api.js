874c7f1cc9d8b0a592e501a6a75e693aa0e2fe71
Jake Glans <165740770+Jake01g@users.noreply.github.com>
2024-12-13 00:44:50 -0700

    check

---
+++ src/components/Api.js
@@ -0,0 +1,41 @@
+class Api {
+  constructor(options) {
+    // constructor body
+  }
+
+  getInitialCards() {
+    // ...
+  }
+
+  // other methods for working with the API
+}
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "00971118-9fba-418f-9a4a-7fa64165e057",
+    "Content-Type": "application/json",
+  },
+});
+
+getInitialCards() {
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      headers: {
+        authorization: "00971118-9fba-418f-9a4a-7fa64165e057"
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+      });
+  }
+
+/*
+user	
+name	"Placeholder name"
+about	"Placeholder description"
+avatar	"https://practicum-content.s3.amazonaws.com/resources/avatar_placeholder_1704989734.svg"
+_id	"a988e0a55355bcf24972e865"
+token	"00971118-9fba-418f-9a4a-7fa64165e057"
+*/
